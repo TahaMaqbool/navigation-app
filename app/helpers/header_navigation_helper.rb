5ce679688c9e181a3783b9d294ca467865096a67
taha <taha.maqbool@talenthouse.com>
2023-02-20 14:12:41 +0500

    fix original implementation and refactored more with specs

--- app/helpers/header_navigation_helper.rb
+++ app/helpers/header_navigation_helper.rb
@@ -1,63 +1,68 @@
 module HeaderNavigationHelper
- APP_URL = 'http://localhost:3000'
-
+ APP_URL = 'http://www.my_app.test'
  def nav_primary(client: false)
-  nav_items = [
-  { text: 'Find Projects', url: contests_path },
-  { text: 'How it works', url: profiles_path },
-  { text: 'Blog', url: "#{APP_URL}/blog" }
-  ]
-
-  unless current_user.nil?
-   nav_items = [
-   { text: 'My Projects', url: my_projects_path },
-   { url: inbox_path, block: "Messages#{unread_messages_count_html}".html_safe }
-   ]
-   nav_items.unshift({ text: 'My Company', url: company_dashboard_path }) if current_user.client?
-   nav_items[0] = { text: 'Find Projects', url: contests_path } unless current_user.client?
+  nav = []
+  if current_user
+   nav.push({ text: 'My Projects', url: my_projects_path })
+   nav.push({ url: inbox_path, block: inbox_block })
+   nav.unshift(client ? company_link : find_projects_link)
+  else
+   nav.push({ text: 'How it works', url: profiles_path })
+   nav.push({ text: 'Blog', url: "#{APP_URL}/blog" })
+   nav.push(client ? about_us_link : browse_projects_link)
   end
-
-  nav_items.insert(1, { text: 'About Us', url: "#{APP_URL}/about" }) if client && current_user.nil?
-
-  nav_items
+  nav
  end
 
  def nav_secondary
-  nav_items = []
+  return [] unless current_user
 
-  unless current_user.nil?
-   nav_items = [
-   { text: 'My Profile', url: profile_path(current_user.profile) },
-   { text: 'Settings', url: edit_profile_path(current_user.profile) },
-   { text: "Find #{current_user.client? ? 'Creatives' : 'Collaborators'}", url: profiles_path }
-   ]
-   nav_items.unshift({ text: 'Latest Ideas', url: contest_ideas_path(current_user.jury_in_contest) }) unless current_user.jury_in_contest.nil?
-   nav_items.push(excarnate_or_logout_link)
-  end
-
-  nav_items
+  nav = [
+    { text: 'My Profile', url: profile_path(current_profile) },
+    { text: 'Settings', url: edit_profile_path(current_profile) },
+    { text: "Find #{current_user.client? ? 'Creatives' : 'Collaborators'}", url: profiles_path }
+  ]
+  nav.unshift(latest_ideas_link) if current_user.jury_in_contest
+  nav.push(original_user ? excarnate_link : logout_link)
+  nav
  end
 
  private
-
- def current_user
-  @current_user ||= User.find_by(id: session[:user_id])
+ def inbox_block
+  "Messages#{unread_messages_block}"
  end
 
- def original_user
-  @original_user ||= User.find_by(id: session[:original_user_id])
+ def unread_messages_block
+  current_user.unread_messages.present? ?
+  " <span class=\"unread-messages\">#{current_user.unread_messages.count}</span>".html_safe : ''
  end
 
- def unread_messages_count_html
-  count = current_user.unread_messages.count
-  count > 0 ? " <span class=\"unread-messages\">#{count}<span>" : ''
+ def company_link
+  { text: 'My Company', url: company_dashboard_path }
  end
 
- def excarnate_or_logout_link
-  if original_user
-   { text: '☯ Excarnate', url: unpretend_path, options: { style: 'color: green' } }
-  else
-   { text: 'Log out', url: logout_path }
-  end
+ def find_projects_link
+  { text: 'Find Projects', url: contests_path }
  end
+
+ def browse_projects_link
+  { text: 'Browse Projects', url: contests_path }
+ end
+
+ def about_us_link
+  { text: 'About Us', url: "#{APP_URL}/about" }
+ end
+
+ def latest_ideas_link
+  { text: 'Latest Ideas', url: contest_ideas_path(current_user.jury_in_contest) }
+ end
+
+ def excarnate_link
+  { text: '☯ Excarnate', url: unpretend_path, options: { style: 'color: green' } }
+ end
+
+ def logout_link
+  { text: 'Log out', url: logout_path }
+ end
+ def current_user; end; def current_profile; end; def original_user; end; def unpretend_path; end
 end
--- spec/helpers/header_navigation_helper_spec.rb
+++ spec/helpers/header_navigation_helper_spec.rb
@@ -1,71 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe HeaderNavigationHelper, type: :helper do
- before(:each) do
-  allow(helper).to receive(:session).and_return({})
- end
-
  describe "#nav_primary" do
-  it "returns nav items for non-logged-in client" do
-   nav_items = helper.nav_primary(client: true)
-   expect(nav_items.size).to eq(4)
-   expect(nav_items).to include(text: "About Us", url: "#{HeaderNavigationHelper::APP_URL}/about")
-   expect(nav_items).to include(text: "Find Projects", url: "/contests")
-   expect(nav_items).to include(text: "How it works", url: "/profiles")
-   expect(nav_items).to include(text: "Blog", url: "#{HeaderNavigationHelper::APP_URL}/blog")
+  context "when user is logged in" do
+   before do
+    allow(helper).to receive_message_chain(:current_user, :unread_messages).and_return([])
+   end
+
+   it "returns nav items for client" do
+    allow(helper).to receive_message_chain(:current_user, :client?).and_return(true)
+    nav_items = helper.nav_primary(client: true)
+    expect(nav_items.size).to eq(3)
+    expect(nav_items).to include(text: "My Company", url: "/company_dashboard")
+    expect(nav_items).to include(text: "My Projects", url: "/my_projects")
+    expect(nav_items).to include(url: "/inbox", block: "Messages")
+   end
+
+   it "returns nav items for non-client" do
+    allow(helper).to receive_message_chain(:current_user, :client?).and_return(false)
+    nav_items = helper.nav_primary
+    expect(nav_items.size).to eq(3)
+    expect(nav_items).to include(text: "Find Projects", url: "/contests")
+    expect(nav_items).to include(text: "My Projects", url: "/my_projects")
+    expect(nav_items).to include(url: "/inbox", block: "Messages")
+   end
   end
 
-  it "returns nav items for logged-in client" do
-   user = double("User", client?: true, unread_messages: [])
-   allow(helper).to receive(:current_user).and_return(user)
+  context "when user is logged out" do
+   before do
+    allow(helper).to receive(:current_user).and_return(nil)
+   end
+   it "returns nav items for client" do
+    nav_items = helper.nav_primary(client: true)
+    expect(nav_items.size).to eq(3)
+    expect(nav_items).to include(text: "How it works", url: "/profiles")
+    expect(nav_items).to include(text: "About Us", url: "#{HeaderNavigationHelper::APP_URL}/about")
+    expect(nav_items).to include(text: "Blog", url: "#{HeaderNavigationHelper::APP_URL}/blog")
+   end
 
-   nav_items = helper.nav_primary
-   expect(nav_items.size).to eq(3)
-   expect(nav_items).to include(text: "My Company", url: "/company_dashboard")
-   expect(nav_items).to include(text: "My Projects", url: "/my_projects")
-   expect(nav_items).to include(url: "/inbox", block: "Messages")
-  end
-
-  it "returns nav items for logged-in non-client user" do
-   user = double("User", client?: false, unread_messages: [])
-   allow(helper).to receive(:current_user).and_return(user)
-
-   nav_items = helper.nav_primary
-
-   expect(nav_items.size).to eq(2)
-   expect(nav_items).to include(text: "Find Projects", url: "/contests")
-   expect(nav_items).to include(url: "/inbox", block: "Messages")
-  end
-
-  it "returns nav items for logged-in user with unread messages" do
-   user = double("User", client?: false, unread_messages: [double("Message"), double("Message")])
-   allow(helper).to receive(:current_user).and_return(user)
-
-   nav_items = helper.nav_primary
-
-   expect(nav_items.size).to eq(2)
-   expect(nav_items).to include(text: "Find Projects", url: "/contests")
-   expect(nav_items).to include(url: "/inbox", block: "Messages <span class=\"unread-messages\">2<span>")
+   it "returns nav items for non-client" do
+    nav_items = helper.nav_primary(client: false)
+    expect(nav_items.size).to eq(3)
+    expect(nav_items).to include(text: "How it works", url: "/profiles")
+    expect(nav_items).to include(text: "Browse Projects", url: "/contests")
+    expect(nav_items).to include(text: "Blog", url: "#{HeaderNavigationHelper::APP_URL}/blog")
+   end
   end
  end
 
- describe "#nav_secondary" do
-  it "returns empty nav items for non-logged-in user" do
-   nav_items = helper.nav_secondary
+ describe  "#nav_secondary" do
+  context "when user is logged in" do
+   before do
+    allow(helper).to receive_message_chain(:current_user, :jury_in_contest).and_return(false)
+    allow(helper).to receive(:current_profile).and_return({ id: 1 })
+    allow(helper).to receive_message_chain(:current_user, :client?).and_return(false)
+   end
 
-   expect(nav_items).to be_empty
+   it "returns nav items for original user" do
+    allow(helper).to receive(:original_user).and_return(true)
+    nav_items = helper.nav_secondary
+    expect(nav_items.size).to eq(4)
+    expect(nav_items).to include(text: "My Profile", url: "/profiles/1")
+    expect(nav_items).to include(text: "Settings", url: "/profiles/1/edit")
+    expect(nav_items).to include(text: "Find Collaborators", url: "/profiles")
+    expect(nav_items).to include(text: "☯ Excarnate", url: nil, options: { style: "color: green" })
+   end
+
+   it "returns nav items for non-original user" do
+    allow(helper).to receive(:original_user).and_return(false)
+    nav_items = helper.nav_secondary
+    expect(nav_items.size).to eq(4)
+    expect(nav_items).to include(text: "My Profile", url: "/profiles/1")
+    expect(nav_items).to include(text: "Settings", url: "/profiles/1/edit")
+    expect(nav_items).to include(text: "Find Collaborators", url: "/profiles")
+    expect(nav_items).to include(text: "Log out", url: "/logout")
+   end
   end
 
-  it "returns nav items for logged-in user who is not a contest judge" do
-   user = double("User", client?: false, profile: '1', profiles: double("Profile"), jury_in_contest: nil, unread_messages: [])
-   allow(helper).to receive(:current_user).and_return(user)
+  context "when user is logged out" do
+   before do
+    allow(helper).to receive(:current_user).and_return(nil)
+   end
 
-   nav_items = helper.nav_secondary
-   expect(nav_items.size).to eq(4)
-   expect(nav_items).to include(text: "My Profile", url: "/profiles/#{user.profile}")
-   expect(nav_items).to include(text: "Settings", url: "/profiles/#{user.profile}/edit")
-   expect(nav_items).to include(text: "Find Collaborators", url: "/profiles")
-   expect(nav_items).to include(text: "Log out", url: "/logout")
+   it "returns empty array" do
+    nav_items = helper.nav_secondary
+    expect(nav_items).to eq([])
+   end
   end
  end
 end
